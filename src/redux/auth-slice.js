5b4ea4537be4348e959fcedbcdae6f2afddb93fe
vitalikzahar <vitalikzahar29@gmail.com>
2023-10-09 00:02:58 +0300

    end

--- src/redux/auth-slice.js
+++ src/redux/auth-slice.js
@@ -24,10 +24,10 @@ const authSlice = createSlice({
         state.token = action.payload.token;
         state.isLoggedIn = true;
       })
-      .addCase(logOut.fulfilled, (state, action) => {
-        state.user = action.payload.user;
-        state.token = action.payload.token;
-        state.isLoggedIn = true;
+      .addCase(logOut.fulfilled, state => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
       })
       .addCase(refreshUser.pending, state => {
         state.isRefreshing = true;
